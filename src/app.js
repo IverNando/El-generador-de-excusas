b8fdaa5dbb41a6fdbd78e81188b8f6b2705df1a6
IverNando <hcarrassets@gmail.com>
2025-09-21 12:35:00 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+function excusas () {
+  let who = ['El gato', 'La abuela', 'Un pirata', 'Mi roomie'];
+  let action = ['se comió', 'quemó', 'apostó y perdió', 'escondió'];
+  let what = ['mi ordenador', 'mis ahorros', 'la bicicleta','el bocadillo','las llaves de la casa'];
+  let when = ['esta noche', 'mientras veia al jujalag', 'ayer, durante la cena', ', eso fue esta tarde, lo juro.'];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function excusaRandom (situaciones) {
+  return situaciones [Math.floor(Math.random() * situaciones.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+}
+
+return (
+  excusaRandom (who) + ' ' +
+  excusaRandom (action) + ' ' + 
+  excusaRandom (what) + ' ' +
+  excusaRandom (when)  
+); 
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const excusaElemento = document.getElementById ('excusa');
+  if (excusaElemento) {
+    excusaElemento.textContent = excusas () ;
+  }
+});
+  
